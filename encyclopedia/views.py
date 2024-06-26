f63b79f259a4857d8cb9e7e9dafeaa11dddf5210
Felipe-dot <felipeluis610@gmail.com>
2024-06-26 18:06:56 -0300

    Create wiki view

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,3 +1,4 @@
+import markdown2
 from django.shortcuts import render
 
 from . import util
@@ -6,5 +7,20 @@ from . import util
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+    })
+
+def wiki(request,title):
+    content = util.get_entry(title)
+
+    if content is None:
+        return render(request, "encyclopedia/error.html", {
+             "message": "The requested page was not found."
+        })
+    
+    html_content = markdown2.markdown(content)
+
+    return render(request, "encyclopedia/content.html", {
+        "title": title,
+        "entry": html_content
     })
 
